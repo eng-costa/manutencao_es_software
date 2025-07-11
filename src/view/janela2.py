5415e64db58edfedaff2e8bc5104b0cac74f2d9f
NarAlves <nara.alves575@gmail.com>
2025-07-11 08:49:38 -0300

    m-corretiva(#6) - Atualizar status de um pedido
    Correção de comparações e validações: ajuste da comparação do menu para inteiro (q == 3) e validação explícita do status do pedido, evitando erros lógicos e entradas inválidas.

--- src/view/janela2.py
+++ src/view/janela2.py
@@ -43,7 +43,7 @@ class Janela2:
             print(f'\nPedidos \n\n{exibir_tela}')
             print(f'Faturamento R$ {faturamento}')
         
-        elif q=='3':
+        elif q==3:
             indice = int(input('Indice do pedido: '))
             resume = ItemControler.search_into_itens_pedidos_id(database_name, indice)
             quantidade_itens = len(resume)
@@ -57,7 +57,7 @@ class Janela2:
                 print('Informações do Pedido\n')
                 print(f'Status: {informacoes_pedido[1]}\nDelivery: {informacoes_pedido[2]}\nEndereco: {informacoes_pedido[3]}\nData: {informacoes_pedido[4]}\nR$ {informacoes_pedido[5]}')
                 novo_status = int(input('preparo - 1 | pronto - 2 | entregue - 3: '))
-                if novo_status/novo_status != 1:
+                if novo_status != 1 and novo_status != 2 and novo_status != 3:
                     print('Entrada inválida, retornando')
                 else:
                     result = PedidoControler.update_pedido_status_id(database_name, indice, novo_status)
