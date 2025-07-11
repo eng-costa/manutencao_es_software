df7d986c219f5e67645bb0c471e197a6d3d10e59
VirginiaMond <virginiamariamf@gmail.com>
2025-07-10 21:32:24 -0300

    m-adaptativa(#7)-criação de nova view: arquivo janela3.py

---
+++ src/view/janela3.py
@@ -0,0 +1,35 @@
+#correção do #7 - nova view 
+
+from pathlib import Path
+import sys
+
+file = Path(__file__).resolve()
+parent, root = file.parent, file.parents[1]
+sys.path.append(str(root))
+
+from model.item import Item
+from controler.itemControler import ItemControler
+
+class Janela3:
+    
+    @staticmethod
+    def mostrar_janela3(database_name: str) -> None:
+        print('------Cadastrar Novo Item no Menu------\n')
+        
+        nome = input('Nome do item: ')
+        tipo = input('Tipo (ex: pizza, bebida): ')
+        descricao = input('Descrição: ')
+        
+        try:
+            preco = float(input('Preço (ex: 29.90): '))
+        except ValueError:
+            print('Preço inválido. Operação cancelada.')
+            return
+
+        item = Item(nome, preco, tipo, descricao)
+        sucesso = ItemControler.insert_into_item(database_name, item)
+
+        if sucesso == True:
+            print('\nItem cadastrado com sucesso!')
+        else:
+            print('\nErro ao cadastrar item.')
